0d7836110487d935ce21abc686f6586ac5aa004e
siamparvez44 <siamparvez44@gmail.com>
2024-12-26 16:22:15 +0600

    [FIX] magic ui items

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,13 +9,13 @@ const Hero = () => {
           <h1 className="font-display text-2xl font-bold lowercase leading-tight text-black dark:text-white sm:text-4xl lg:text-6xl">
             Coming Soon to LevelUP the World!
           </h1>
-          <p className="text-md mt-6 text-neutral-600 dark:text-neutral-300 sm:text-xl">
+          <p className="text-md mt-6 italic text-neutral-600 dark:text-neutral-300 sm:text-xl">
             {/* This landing Landing page is built specially to{" "}
             <b>save your time</b>. Find the <b>product-market-fit</b> before you
             launch. */}
-            We&apos;re working behind the scenes to bring you innovative IT solutions, training, and services—stay tuned for the launch!
+            We&apos;re working behind the scenes to bring you innovative IT solutions, training, and services — stay tuned for the launch!
           </p>
-          <div className=" mx-auto mt-4 flex w-full max-w-full flex-col items-center justify-center gap-1 py-5">
+          <div className="mx-auto mt-4 flex w-full max-w-full flex-col items-center justify-center gap-1 py-5">
            <p className=" text-sm text-neutral-600 dark:text-neutral-300 sm:text-lg">
             Follow us on
           </p>
