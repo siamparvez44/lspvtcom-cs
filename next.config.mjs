a79003aaa03e6e4f49a8e730514eb95166de7de5
siamparvez44 <siamparvez44@gmail.com>
2025-01-12 00:04:46 +0600

    [TEST] 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['cdn.simpleicons.org'],
-    },
-  };
+const nextConfig = {};
   
   export default nextConfig
   
